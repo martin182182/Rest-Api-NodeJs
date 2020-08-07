5f1dcc8d95d850f20279b732929ec0c924d0e488
martin <martinenrique785@gmail.com>
2020-08-07 16:15:49 -0500

    Backend actualizado

--- controllers/datos.js
+++ controllers/datos.js
@@ -15,13 +15,69 @@ var controller = {
     });
   },
   getClients : async function(req,res){
-    Dato.find({}).skip((2) * 3).limit(4).exec((err,clients)=>{
+    Dato.find({}).exec((err,clients)=>{
       if(err) return res.status(500).send({message: 'Error al devolver los datos.'});
       if(!clients) return res.status(404).send({message: 'No hay clientes para mostrar'});
       return res.status(200).send({clients});
     });
+  },
+  getClient: function(req, res){
+		var clientId = req.params.id;
+
+		if(clientId == null) return res.status(404).send({message: 'El cliente no existe.'});
+
+		Dato.findById(clientId, (err, client) => {
+
+			if(err) return res.status(500).send({message: 'Error al devolver los datos.'});
+
+			if(!client) return res.status(404).send({message: 'El cliente no existe.'});
+
+			return res.status(200).send({
+			     client
+			});
+
+		});
+	},
+  updateClient: function(req, res){
+    var clientId = req.params.id;
+    var update = req.body;
+
+    Dato.findByIdAndUpdate(clientId, update, {new:true}, (err, clientUpdated) => {
+      if(err) return res.status(500).send({message: 'Error al actualizar'});
+
+      if(!clientUpdated) return res.status(404).send({message: 'No existe el cliente para actualizar'});
+
+      return res.status(200).send({
+        message: 'Cliente actualizado'
+      });
+    });
+
+  },
+
+  deleteClient: function(req, res){
+    var clientId = req.params.id;
+
+    Dato.findByIdAndRemove(clientId, (err, clientRemoved) => {
+      if(err) return res.status(500).send({message: 'No se ha podido borrar el cliente'});
+
+      if(!clientRemoved) return res.status(404).send({message: "No se puede eliminar ese cliente"});
+
+      return res.status(200).send({
+        message: "Cliente borrado"
+      });
+    });
   }
+
+
+
+
+
+
+
+
+
 }
+
 
 
 module.exports = controller;
--- routes/datos.js
+++ routes/datos.js
@@ -7,4 +7,7 @@ var router = express.Router();
 
 router.post('/nuevoCliente',datosController.saveClient);
 router.get('/clientes',datosController.getClients);
+router.get('/clientes/:id', datosController.getClient);
+router.put('/clientes/:id', datosController.updateClient);
+router.delete('/clientes/:id', datosController.deleteClient);
 module.exports = router;
